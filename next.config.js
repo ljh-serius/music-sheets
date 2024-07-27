61b759bf4ae9700d435d40635e3afc66cc151da5
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-07-27 15:43:11 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 const withSitemap = require('next-sitemap/config');
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
