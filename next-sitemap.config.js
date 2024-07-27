dfc59c9f0a65a8fd7ab864032cd3b4949f2b9273
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-07-27 16:42:46 +0200

    ok

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,5 @@
 module.exports = {
     siteUrl: 'https://strum.fun', // Replace with your actual site URL
     generateRobotsTxt: true, // Generate a robots.txt file
-    outDir: './public', // Directory where the sitemap will be generated
-    sitemapName: 'sitemap.xml', // Name of the sitemap file
-    generateIndexSitemap: false, // Generate a single sitemap file instead of index sitemap
-    robotsTxtOptions: {
-      additionalSitemaps: [
-        'https://strum.fun/sitemap.xml',
-      ],
-    },
   };
   
