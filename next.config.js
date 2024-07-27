9db8cc659ffdbbeebf10fa1e03c84f558ada6f59
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-07-27 16:23:30 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
