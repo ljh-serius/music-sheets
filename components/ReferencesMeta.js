dbc1cf7626508a3d76bd6db90c55585b034d33a9
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-08-01 13:31:35 +0200

    ads.txt

--- components/ReferencesMeta.js
+++ components/ReferencesMeta.js
@@ -7,6 +7,7 @@ export default function Meta({title}){
             <meta name="keywords" content="keys, sharps, scales, modes, arpeggios, C major, G major, D major, A major, E major, B major, F# major, C# major, F major, Bb major, Eb major, Ab major, Db major, Gb major, Cb major, natural minor, harmonic minor, melodic minor, dorian, phrygian, lydian, mixolydian, aeolian, locrian, major arpeggios, minor arpeggios, diminished arpeggios, augmented arpeggios" />
             <meta name="description" content="Explore comprehensive references for musical keys, scales, modes, and arpeggios. Find detailed information and resources for all keys, sharps, scales, modes, and arpeggios to enhance your musical knowledge." />
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6308607102543154" crossorigin="anonymous"></script>
+            <meta name="google-adsense-account" content="ca-pub-6308607102543154" />
         </Head>
     );
 }
