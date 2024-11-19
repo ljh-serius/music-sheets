1dfa5dde5cc6d66413c47521376810a18164e23c
ljh.serius <h.eljaouhari.code@gmail.com>
2024-11-19 14:59:52 +0100

    Great

--- components/Partials/Head.js
+++ components/Partials/Head.js
@@ -4,21 +4,6 @@ import Script from 'next/script';
 export default function Meta({ title }) {
     return (
         <Head>
-            <script>
-                (function(w, d, s, l, i) {
-                    w[l] = w[l] || [];
-                    w[l].push({
-                        'gtm.start': new Date().getTime(),
-                        event: 'gtm.js' 
-                    });
-                    var f = d.getElementsByTagName(s)[0],
-                    j = d.createElement(s),
-                    dl = l !== 'dataLayer' ? '&l=' + l : '';
-                    j.async = true;
-                    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                    f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', 'GTM-PBX6GPRP');
-            </script>
             <title>{title}</title>
             <meta
                 name="format-detection"
@@ -32,6 +17,14 @@ export default function Meta({ title }) {
                 name="description"
                 content="Explore comprehensive references for musical keys, scales, modes, and arpeggios. Find detailed information and resources for all keys, sharps, scales, modes, and arpeggios to enhance your musical knowledge."
             />
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-5FJXJGLWWH"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){ dataLayer.push(arguments) }
+                gtag('js', new Date());
+
+                gtag('config', 'G-5FJXJGLWWH');
+            </script>
             <script
                 async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3419259043892692"
