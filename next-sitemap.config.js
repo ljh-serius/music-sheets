9599751f020f4ac5dfb68dd89ee8cb50ac357527
ljh.serius <h.eljaouhari.code@gmail.com>
2024-11-19 04:13:48 +0100

    hope so

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
-// module.exports = {
-//     siteUrl: 'https://strum.fun', // Replace with your actual site URL
-//     generateRobotsTxt: true, // Generate a robots.txt file
-//   };
+module.exports = {
+    siteUrl: 'https://strum.fun', // Replace with your actual site URL
+    generateRobotsTxt: true, // Generate a robots.txt file
+  };
   
