5e1db2e4ad657f1ac902cb55bf76208ae98ea10d
ljh-serius <34250925+ljh-serius@users.noreply.github.com>
2024-11-21 02:55:01 +0100

    great

--- components/Partials/Head.js
+++ components/Partials/Head.js
@@ -17,30 +17,12 @@ export default function Meta({ title }) {
                 content="Explore comp
                 rehensive references for musical keys, scales, modes, and arpeggios. Find detailed information and resources for all keys, sharps, scales, modes, and arpeggios to enhance your musical knowledge."
             />
-
-            <script>
-                /* eslint-disable */
-                (function(w, d, s, l, i) {
-                    w[l] = w[l] || [];
-                    w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js' });
-                    var f = d.getElementsByTagName(s)[0],
-                    j = d.createElement(s),
-                    dl = l != 'dataLayer' ? '&l=' + l : '';
-                    j.async = true;
-                    j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' +
-                    i +
-                    dl +
-                    '&gtm_auth=UzCmdBMmDTt9sC8Daxg1Xw&gtm_preview=env-1&gtm_cookies_win=x';
-                    f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', 'GTM-PBX6GPRP');
-            </script>
-
             <script
                 async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3419259043892692"
                 crossOrigin="anonymous"
             ></script>
+            <meta name="google-site-verification" content="_DI-fYrpMShM8WqrOzlW7z2MI2E3q8CEVtyZ5usRGms" />
         </Head>
     );
 }
