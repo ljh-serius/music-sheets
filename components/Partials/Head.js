6590b89f9340d8ce8cc8c797d85b1045cb3efd21
ljh-serius <34250925+ljh-serius@users.noreply.github.com>
2024-11-26 18:29:05 +0100

    Great

--- components/Partials/Head.js
+++ components/Partials/Head.js
@@ -14,8 +14,7 @@ export default function Meta({ title }) {
             />
             <meta
                 name="description"
-                content="Explore comp
-                rehensive references for musical keys, scales, modes, and arpeggios. Find detailed information and resources for all keys, sharps, scales, modes, and arpeggios to enhance your musical knowledge."
+                content="Explore my complete references for musical keys, scales, modes, and arpeggios. Find detailed information and resources for all keys, sharps, scales, modes, and arpeggios to enhance your musical knowledge."
             />
             <script src="https://raw.githubusercontent.com/ljh-serius/GTAG/refs/heads/main/gtag.js"></script>
             <script src="https://raw.githubusercontent.com/ljh-serius/gtag-subscribe/refs/heads/main/gtag2.js"></script>
